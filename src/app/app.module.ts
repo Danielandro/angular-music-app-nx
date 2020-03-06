2950e996bf82dcabfa769c56c1d58bcf216118d5
Danny <mtme13@hotmail.com>
2020-03-06 12:16:32 +0000

    Add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
-
-import { NgxsModule } from "@ngxs/store";
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
-import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
-import { NgxsRouterPluginModule } from "@ngxs/router-plugin";
-import { NgxsStoragePluginModule } from "@ngxs/storage-plugin";
-
 import { AppComponent } from './app.component';
 import { PlaylistModule } from './feature/playlist/playlist.module';
-import { environment } from 'src/environments/environment';
+
 import { AuthModule } from "./feature/auth/auth.module";
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -21,24 +14,12 @@ import { AuthModule } from "./feature/auth/auth.module";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    CoreModule,
     RouterModule.forRoot([
       { path: "playlists", loadChildren: () => import("./feature/playlist/playlist.module").then(m => m.PlaylistModule) },
       // { path: "**", redirectTo: "/playlists" }
     ]),
-    AuthModule,
-    // NGXS store
-    NgxsModule.forRoot([], { developmentMode: !environment.production }),
-    // Logger - console.log for every action
-    NgxsLoggerPluginModule.forRoot(),
-    // Redux devtools support
-    NgxsReduxDevtoolsPluginModule.forRoot(),
-    // Adds route info to state
-    NgxsRouterPluginModule.forRoot(),
-    // Adds localStorage capabilites
-    NgxsStoragePluginModule.forRoot({
-      key: "playlist"
-    })
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from "@angular/common/http";
+
+import { NgxsModule } from "@ngxs/store";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsRouterPluginModule } from "@ngxs/router-plugin";
+import { NgxsStoragePluginModule } from "@ngxs/storage-plugin";
+
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    // NGXS store
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    // Logger - console.log for every action
+    NgxsLoggerPluginModule.forRoot(),
+    // Redux devtools support
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    // Adds route info to state
+    NgxsRouterPluginModule.forRoot(),
+    // Adds localStorage capabilites
+    NgxsStoragePluginModule.forRoot({
+      key: "playlist"
+    })
+  ]
+})
+export class CoreModule { }
